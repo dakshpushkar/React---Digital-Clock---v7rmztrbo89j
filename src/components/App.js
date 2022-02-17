266e7bf1e551e36384276557db47f93334c1d96c
Your Name <you@example.com>
2022-02-18 00:13:04 +0530

    final commit

--- src/components/App.js
+++ src/components/App.js
@@ -1,10 +1,21 @@
-import React from 'react'
+import React, { useEffect, useState } from 'react'
 import '../styles/App.css';
 const App = () => {
+  const[clock,setClock] = useState(new Date())
+  useEffect(()=>{
+   const interval = setInterval(()=>{
+     setClock(new Date())
+   },1000)
+   return ()=>{
+     clearInterval(interval)
+   }
+  },[clock])
 
   return (
     <div id="main">
-      <div className="date-time"></div>
+      <div className="date-time">
+        {clock.toLocaleTimeString()}
+      </div>
     </div>
   )
 }
